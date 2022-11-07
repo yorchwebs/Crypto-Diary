6371fbbce84e884e0b3dc0998eb688f63a7a96d3
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2022-11-07 15:02:58 -0600

    Se modificó el archivo HTML, CSS y se elimino el archivo JS

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 
 import cryptocompare
-import time
 
 app = Flask(__name__)
 
@@ -56,6 +55,50 @@ for coin in coins:
         precio_dolar.append('{:,.2f}'.format(coins['DOGE']['USD']))
         precio_peso.append('{:,.2f}'.format(coins['DOGE']['MXN']))
 
+def time():
+
+    while True:
+        for coin in coins:
+            if coin == 'BTC':
+                moneda.append(coin)
+                precio_dolar.append('{:,.2f}'.format(coins['BTC']['USD']))
+                precio_peso.append('{:,.2f}'.format(coins['BTC']['MXN']))
+            if coin == 'ETH':
+                moneda.append(coin)
+                precio_dolar.append('{:,.2f}'.format(coins['ETH']['USD']))
+                precio_peso.append('{:,.2f}'.format(coins['ETH']['MXN']))
+            if coin == 'USDT':
+                moneda.append(coin)
+                precio_dolar.append('{:,.2f}'.format(coins['USDT']['USD']))
+                precio_peso.append('{:,.2f}'.format(coins['USDT']['MXN']))
+            if coin == 'USDC':
+                moneda.append(coin)
+                precio_dolar.append('{:,.2f}'.format(coins['USDC']['USD']))
+                precio_peso.append('{:,.2f}'.format(coins['USDC']['MXN']))
+            if coin == 'BNB':
+                moneda.append(coin)
+                precio_dolar.append('{:,.2f}'.format(coins['BNB']['USD']))
+                precio_peso.append('{:,.2f}'.format(coins['BNB']['MXN']))
+            if coin == 'XRP':
+                moneda.append(coin)
+                precio_dolar.append('{:,.2f}'.format(coins['XRP']['USD']))
+                precio_peso.append('{:,.2f}'.format(coins['XRP']['MXN']))
+            if coin == 'BUSD':
+                moneda.append(coin)
+                precio_dolar.append('{:,.2f}'.format(coins['BUSD']['USD']))
+                precio_peso.append('{:,.2f}'.format(coins['BUSD']['MXN']))
+            if coin == 'ADA':
+                moneda.append(coin)
+                precio_dolar.append('{:,.2f}'.format(coins['ADA']['USD']))
+                precio_peso.append('{:,.2f}'.format(coins['ADA']['MXN']))
+            if coin == 'SOL':
+                moneda.append(coin)
+                precio_dolar.append('{:,.2f}'.format(coins['SOL']['USD']))
+                precio_peso.append('{:,.2f}'.format(coins['SOL']['MXN']))
+            if coin == 'DOGE':
+                moneda.append(coin)
+                precio_dolar.append('{:,.2f}'.format(coins['DOGE']['USD']))
+                precio_peso.append('{:,.2f}'.format(coins['DOGE']['MXN']))
 
 monedas = moneda.copy()
 nombres = nombre.copy()
@@ -65,7 +108,7 @@ pesos = precio_peso.copy()
 @app.route('/')
 def index():
 
-    return render_template('index.html', monedas=monedas, nombres=nombres, dolares=dolares, pesos=pesos)
+        return render_template('index.html', monedas=monedas, nombres=nombres, dolares=dolares, pesos=pesos)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
