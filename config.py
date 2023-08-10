4daddb0e5eb3048381c5e77713b4275c3f5eb921
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-08-10 12:17:45 -0600

    (Add manage, config): Added config & manage files to run APP

---
+++ config.py
@@ -0,0 +1,15 @@
+from decouple import config
+
+class Config:
+    SECRET_KEY = config('SECRET_KEY')
+    CRYPTOCOMPARE_API_KEY = config("CRYPTO_COMPARE_KEY")
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
---
+++ manage.py
@@ -0,0 +1,8 @@
+from app import create_app
+from config import config
+
+config_name = config["development"]
+app = create_app(config_name)
+
+if __name__ == "__main__":
+    app.run()
