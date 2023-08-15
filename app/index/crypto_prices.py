6c6368365dee4301c4b8c8af05197d4c9645af6b
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-08-14 20:28:39 -0600

    (Modify methods): Modified Methods for update "prices"

--- app/index/crypto_prices.py
+++ app/index/crypto_prices.py
@@ -1,23 +1,22 @@
-import time
 import cryptocompare
 
 from typing import List
-from app.index.utils.price_format import price_formatter
 
 from concurrent.futures import ThreadPoolExecutor
 
-start = time.time()
+from app.index.utils.price_format import price_formatter
 
 
-class CryptoPrices:
-    def __init__(self, coins: dict = None, names: dict = None):
+class CryptoPrice:
+    def __init__(self):
         self.coins = cryptocompare.get_price(
-            ["BTC", "ETH", "LTC", "BCH", "XRP", "BSV", "EOS", "XLM", "ADA", "TRX"],
+            ["BTC", "ETH", "USDT", "USDC", "BNB", "XRP", "BUSD", "ADA", "SOL", "DOGE"],
             ["USD", "MXN"],
         )
 
-    def get_prices(self) -> dict:
-        symbol_list: List[str] = [
+    @property
+    def prices(self) -> dict:
+        SYMBOL_LIST: List[str] = [
             "BTC",
             "ETH",
             "USDT",
@@ -29,7 +28,7 @@ class CryptoPrices:
             "SOL",
             "DOGE",
         ]
-        name_list: List[str] = [
+        NAME_LIST: List[str] = [
             "Bitcoin",
             "Ethereum",
             "Tether",
@@ -44,27 +43,21 @@ class CryptoPrices:
         usd_price_list: List[float] = []
         mxn_price_list: List[float] = []
 
-        while True:
-            with ThreadPoolExecutor(max_workers=4) as executor:
-                for coin in self.coins:
-                    executor.submit(
-                        usd_price_list.append(getting_price(self.coins[coin]["USD"]))
-                    )
-                    executor.submit(
-                        mxn_price_list.append(getting_price(self.coins[coin]["MXN"]))
-                    )
-
-            break
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            for coin in self.coins:
+                executor.submit(
+                    usd_price_list.append(getting_price(self.coins[coin]["USD"]))
+                )
+                executor.submit(
+                    mxn_price_list.append(getting_price(self.coins[coin]["MXN"]))
+                )
 
         return {
-            "symbol_list": symbol_list,
-            "name_list": name_list,
+            "symbol_list": SYMBOL_LIST,
+            "name_list": NAME_LIST,
             "usd_price_list": usd_price_list,
             "mxn_price_list": mxn_price_list,
         }
-
-
-print(time.time() - start)
 
 
 @price_formatter
