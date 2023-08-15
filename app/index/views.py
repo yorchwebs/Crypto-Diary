771fd904e735faccb66b1d29091a5ce507ab0197
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-08-14 20:31:16 -0600

    (Add Form): Added form validate and save data

--- app/index/views.py
+++ app/index/views.py
@@ -1,19 +1,41 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
 
-from app.index.crypto_prices import CryptoPrices
+
+from app.index.forms import NewsletterForm
+from app.index.models import Newsletter
+from app.index.crypto_prices import CryptoPrice
+from app.index.utils.pydantic_validator import NewsletterModel
+
 
 index_bp = Blueprint(
     "index_bp", __name__, template_folder="templates", static_folder="static"
 )
 
 
-# Ruta y función para obtener datos.
-@index_bp.route("/")
+@index_bp.route("/", methods=["GET", "POST"])
 def index():
-        return render_template(
-            "index.html",
-            symbol_list=CryptoPrices().get_prices()["symbol_list"],
-            name_list = CryptoPrices().get_prices()["name_list"],
-            usd_price_list=CryptoPrices().get_prices()["usd_price_list"],
-            mxn_price_list=CryptoPrices().get_prices()["mxn_price_list"],
-        )
+    form = NewsletterForm()
+    if form.validate_on_submit():
+        validate_data = NewsletterModel(name=form.name.data, email=form.email.data)
+        try:
+            new_subscriber = Newsletter.create(
+                name=validate_data.name, email=validate_data.email
+            )
+            new_subscriber.save()
+            flash("Gracias por suscribirte !!")
+            return redirect(url_for("index_bp.index"))
+
+        except Exception:
+            flash("El email ya existe en la base de datos")
+            return redirect(url_for("index_bp.index"))
+
+    crypto_prices = CryptoPrice().prices
+
+    return render_template(
+        "index.html",
+        form=form,
+        symbol_list=crypto_prices["symbol_list"],
+        name_list=crypto_prices["name_list"],
+        usd_price_list=crypto_prices["usd_price_list"],
+        mxn_price_list=crypto_prices["mxn_price_list"],
+    )
