7ccc6127ed867abf151e0e7383754c0ef80c2e43
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-08-16 19:55:45 -0600

    (Add emails conection): Added emails.Python connection to run send_emails method

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,43 @@
 from flask import Flask
 
-from app.index.models import Newsletter
+from flask_mail import Mail
+
+from datetime import datetime
 
 from app.db.database import database
 
 from app.index.views import index_bp
 
-# Iniciamos la Aplicación.
+from flask_wtf.csrf import CSRFProtect
+
+from app.emails.emails import send_email
+
+from app.emails.models import Newsletter
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
 app = Flask(__name__)
+
+mail = Mail(app)
 
 app.register_blueprint(index_bp)
 
+csrf = CSRFProtect()
+
+scheduler = BackgroundScheduler()
+
+
 def create_app(config):
-    app.config.from_object(config)
-    # csrf.init_app(app)
-    # app.register_error_handler(404, page_not_found)
-    database.drop_tables([Newsletter])
-    database.create_tables([Newsletter])
-    return app
+    try:
+        csrf.init_app(app)
+        app.config.from_object(config)
+        database.create_tables([Newsletter])
+        scheduler.add_job(
+            send_email, "interval", minutes=1, next_run_time=datetime.now()
+        )
+        scheduler.start()
+
+        return app
+
+    except Exception as e:
+        return str(e)
