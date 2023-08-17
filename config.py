6546bf5e791b7457d643daa95a908898e1ff189d
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-08-16 19:49:36 -0600

    (Add conf. to connect email): Added a data configuration to connect email
    Added a data configuration to connect email service to send

--- config.py
+++ config.py
@@ -3,7 +3,12 @@ from decouple import config
 class Config:
     SECRET_KEY = config('SECRET_KEY')
     CRYPTOCOMPARE_API_KEY = config("CRYPTO_COMPARE_KEY")
-
+    MAIL_SERVER = config("MAIL_SERVER")
+    MAIL_PORT = config("MAIL_PORT")
+    MAIL_USE_TLS = config("MAIL_USE_TLS")
+    MAIL_USERNAME = config("MAIL_USERNAME")
+    MAIL_PASSWORD = config("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = config("MAIL_DEFAULT_SENDER")
 
 class DevelopmentConfig(Config):
     DEBUG = True
