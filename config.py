fa1c3616793b5c2953795149395faf8f07d22973
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-08-21 14:47:54 -0600

    (Delete email config): Deleted email config because change the data conection to emails file
    Added emails connection for programming and run the send emails app

--- config.py
+++ config.py
@@ -3,12 +3,6 @@ from decouple import config
 class Config:
     SECRET_KEY = config('SECRET_KEY')
     CRYPTOCOMPARE_API_KEY = config("CRYPTO_COMPARE_KEY")
-    MAIL_SERVER = config("MAIL_SERVER")
-    MAIL_PORT = config("MAIL_PORT")
-    MAIL_USE_TLS = config("MAIL_USE_TLS")
-    MAIL_USERNAME = config("MAIL_USERNAME")
-    MAIL_PASSWORD = config("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = config("MAIL_DEFAULT_SENDER")
 
 class DevelopmentConfig(Config):
     DEBUG = True
@@ -16,5 +10,5 @@ class DevelopmentConfig(Config):
 
 config = {
     'development': DevelopmentConfig,
-    'default': DevelopmentConfig
+    'default': DevelopmentConfig,
 }
