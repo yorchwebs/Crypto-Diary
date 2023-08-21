b7fd3b01f2279a7ddd09571850b502b987548c9d
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-08-21 14:50:17 -0600

    (Move file): Moved file: index to emails

---
+++ app/index/models.py
@@ -0,0 +1,16 @@
+import peewee
+
+from app.db.database import database
+
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = database
+
+
+class Newsletter(BaseModel):
+    name = peewee.CharField(max_length=255)
+    email = peewee.CharField(max_length=255, unique=True)
+
+    class Meta:
+        table_name = "subs"
