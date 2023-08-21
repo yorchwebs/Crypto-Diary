e60688cfb3de1fca85949936eacab6b04c1c10d3
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-08-21 14:45:56 -0600

    (Add emails): Added emails conection
    Added emails connection for programming and run the send emails app

--- app/__init__.py
+++ app/__init__.py
@@ -4,43 +4,44 @@ from app.db.database import database
 
 from app.index.views import index_bp
 
+from emails.emails import SenderEmail
+
 from flask_wtf.csrf import CSRFProtect
+
+from app.index.models import Newsletter
 
 from datetime import datetime, timedelta
 
-from app.emails.models import Newsletter
-
-from app.emails.emails import SenderEmail
-
 from apscheduler.schedulers.background import BackgroundScheduler
 
+app = Flask(__name__)
+
 csrf = CSRFProtect()
-
-email = SenderEmail()
-
-send_mail = email.mail
 
 scheduler = BackgroundScheduler()
 
 
-def create_app(config):
-    app = Flask(__name__)
+def send_emails_job():
+    email_sender = SenderEmail()
+    email_sender.send_email()
 
+
+def create_app(config):
     app.config.from_object(config)
 
-    email.init_app(app)
     csrf.init_app(app)
-    email.init_app(app)
 
     app.register_blueprint(index_bp)
 
     database.create_tables([Newsletter])
 
+    send_time = datetime.now() + timedelta(seconds=10)
     scheduler.add_job(
-        send_mail,
+        send_emails_job,
         "interval",
-        minutes=1,
-        next_run_time=datetime.now() + timedelta(seconds=10),
+        days=1,
+        start_date=send_time,
+        id="send_emails_job",
     )
     scheduler.start()
 
